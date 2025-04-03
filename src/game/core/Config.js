34f3f2d84791ac1ba223d2d42a8d6f3c880c87b5
Denner Torres <dennerstorres@icloud.com>
2025-04-03 12:44:19 -0400

    Ajuste de configs

--- src/game/core/Config.js
+++ src/game/core/Config.js
@@ -1,26 +1,26 @@
 export const Config = {
     gridSize: 40,
     path: [
-        {x: 0, y: 3},
-        {x: 7, y: 3},
-        {x: 7, y: 7},
-        {x: 0, y: 7},
-        {x: 0, y: 11},
-        {x: 7, y: 11},
-        {x: 7, y: 15}
+        {x: 0, y: 4},
+        {x: 11, y: 4},
+        {x: 11, y: 1},
+        {x: 15, y: 1},
+        {x: 15, y: 9},
+        {x: 9, y: 9},
+        {x: 9, y: 15}
     ],
     initialMoney: 100,
     initialLives: 10,
     initialWave: 1,
     initialEnemiesPerWave: 5,
-    towerCost: 50,
+    towerCost: 40,
     towerRange: 150,
-    towerDamage: 10,
+    towerDamage: 18,
     towerCooldown: 1000,
-    enemyHealth: 100,
-    enemySpeed: 2,
-    enemySpeedIncrease: 0.5,
-    waveEnemyIncrease: 2,
-    waveMoneyReward: 50,
-    waveMoneyIncrease: 10
+    enemyHealth: 110,
+    enemySpeed: 1.6,
+    enemySpeedIncrease: 0.6,
+    waveEnemyIncrease: 4,
+    waveMoneyReward: 40,
+    waveMoneyIncrease: 8
 }; 
